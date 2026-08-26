dab5256ebd501cec6cbe12b5d5e4b1b5c22e09b5
Your Name <you@example.com>
2026-08-26 15:57:51 +0530

    sql till assignment 3

--- QS SQL/SQL Week1.sql
+++ QS SQL/SQL Week1.sql
@@ -42,6 +42,7 @@ INSERT INTO EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO) VALUES
 (1019, 'THOMAS',   'SUPPORT',      7698, '2024-01-18', 2200, 200,  30),
 (1020, 'JACKSON',  'DATA ANALYST', 7566, '2024-02-05', 3200, NULL, 20);
 
+-- =================ASSIGNMENT NO. 1===========================
 
 -- ============================================================
 -- 1. WAQTD name of the employee along with their annual salary.
@@ -124,6 +125,8 @@ SELECT ENAME
 FROM EMP
 WHERE HIREDATE='2023-04-19';
 
+-- ======================ASSIGNMENT NO 2===========================
+
 -- =========================== QUESTION 1==========================
 SELECT ENAME, SAL*12 AS ANNUALSAL
 FROM EMP
@@ -192,5 +195,150 @@ WHERE JOB='ANALYST';
 SELECT *
 FROM EMP
 WHERE SAL>2000;
+
+-- ================CONCETATION=============
+SELECT CONCAT('Mr',' ', ename) AS full_name 
+FROM emp;
+
+-- ===================AND OPERATOR=============
+select ename
+from emp
+where job='CLERK' AND DEPTNO=20;
+
+SELECT *
+FROM EMP
+WHERE JOB='MANAGER' AND SAL>2000;
+
+-- ===================OR OPERATOR=============
+Select ename
+from emp
+where DEPTNO=10 OR DEPTNO=20;
+
+SELECT *
+FROM EMP
+WHERE JOB='MANAGER' OR JOB='ANALYST';
+
+-- ===================NOT OPERATOR==========
+SELECT *
+FROM EMP
+WHERE not DEPTNO=10;
+
+SELECT *
+FROM EMP
+WHERE NOT JOB='SALESMAN';
+
+-- ASSIGNMENT ON LOGICAL OPERATORS
+
+-- 1. WAQTD details of the employees working as CLERK and earning less than 1500.
+SELECT *
+FROM EMP
+WHERE JOB='CLERK' AND SAL>1500;
+
+-- 2. WAQTD name and hiredate of the employees working as MANAGER in DEPT 30.
+SELECT NAME, HIREDATE
+FROM EMP
+WHERE JOB='MANAGER' AND DEPTNO=30;
+
+-- 3. WAQTD details of the employees along with annual salary if they are working in DEPT 30 as SALESMAN and their annual salary has to be greater than 14000.
+SELECT * , SAL*12 ANNUALSAL
+FROM EMP
+WHERE JOB='SALESMAN' AND SAL*12>14000;
+
+-- 4. WAQTD all the details of the employees working in DEPT 30 OR as ANALYST.
+SELECT *
+FROM EMP
+WHERE DEPTNO=30 OR JOB='ANALYST';
+
+-- 5. WAQTD names of the employees whose salary is less than 1100 and their designation is CLERK.
+SELECT ENAME
+FROM EMP
+WHERE SAL>1100 AND JOB='CLERK';
+
+-- 6. WAQTD name, sal, annual sal and deptno if deptno is 20, earning more than 1100 and annual salary exceeds 12000.
+SELECT ENAME, SAL, SAL*12 AS ANNUALSAL, DEPTNO
+FROM EMP
+WHERE DEPTNO=20 AND SAL>1100 AND SAL*12>12000;
+
+-- 7. WAQTD empno and names of the employees working as MANAGER in DEPT 20.
+SELECT EMPNO, ENAMES
+FROM EMP
+WHERE JOB='MANAGER' AND DEPTNO=20;
+
+-- 8. WAQTD details of employees working in DEPT 20 OR 30.
+SELECT *
+FROM EMP
+WHERE DEPTNO=20 OR DEPTNO=30;
+
+
+-- 9. WAQTD details of employees working as ANALYST in DEPT 10.
+SELECT *
+FROM EMP
+WHERE JOB='ANALYST' AND DEPTNO=10;
+
+
+-- 10. WAQTD details of employees working as PRESIDENT with salary of 4000.
+SELECT *
+FROM EMP
+WHERE JOB='PRESIDENT' AND SAL=4000;
+
+
+-- 11. WAQTD names and deptno of employees working as CLERK in DEPT 10 OR 20.
+SELECT ENAME, DEPTNO
+FROM EMP
+WHERE JOB='CLERK' AND (DEPTNO=10 OR DEPTNO=20);
+
+
+-- 12. WAQTD details of employees working as CLERK OR MANAGER in DEPT 10.
+SELECT *
+FROM EMP
+WHERE DEPTNO=10 AND (JOB='CLERK' OR JOB='MANAGER');
+
+
+-- 13. WAQTD names of employees working in DEPT 10, 20, 30, 40.
+SELECT ENAME
+FROM EMP
+WHERE DEPTNO=10 OR DEPTNO=20 OR DEPTNO=30 OR DEPTNO=40;
+
+
+-- 14. WAQTD details of employees with EMPNO 7902, 7839.
+SELECT *
+FROM EMP
+WHERE EMPNO=7902 OR EMPNO=7839;
+
+
+-- 15. WAQTD details of employees working as MANAGER OR SALESMAN OR CLERK.
+SELECT *
+FROM EMP
+WHERE JOB='MANAGER' OR JOB='SALESMAN' OR JOB='CLERK';
+
+
+-- 16. WAQTD names of employees hired AFTER 81 AND BEFORE 87.
+SELECT ENAME
+FROM EMP
+WHERE HIREDATE > '31-DEC-1981' AND HIREDATE < '01-JAN-1987';
+
+
+-- 17. WAQTD details of employees earning more than 1250 but less than 3000.
+SELECT *
+FROM EMP
+WHERE SAL>1250 AND SAL<3000;
+
+
+-- 18. WAQTD names of employees hired AFTER 81 into DEPT 10 OR 30.
+SELECT ENAME
+FROM EMP
+WHERE HIREDATE > '31-DEC-1981' AND (DEPTNO=10 OR DEPTNO=30);
+
+
+-- 19. WAQTD names of employees along with annual salary for the employees working as MANAGER OR CLERK into DEPT 10 OR 30.
+SELECT ENAME, SAL*12 AS ANNUALSAL
+FROM EMP
+WHERE (JOB='MANAGER' OR JOB='CLERK') AND (DEPTNO=10 OR DEPTNO=30);
+
+
+-- 20. WAQTD all the details along with annual salary if SAL is between 1000 AND 4000 AND annual salary is more than 15000.
+SELECT *, SAL*12 AS ANNUALSAL
+FROM EMP
+WHERE SAL>=1000 AND SAL<=4000 AND SAL*12>15000;
 
 SELECT * FROM EMP;
